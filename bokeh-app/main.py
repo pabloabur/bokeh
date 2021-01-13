e245c09b5a699fa31a84dcae9ad9f2bb6e2d9005
pabloabur <pabloabur@gmail.com>
2021-01-13 12:37:10 +1100

    Test with data

--- bokeh-app/main.py
+++ bokeh-app/main.py
@@ -1,89 +1,56 @@
-from os.path import join, dirname
-import datetime
-
-import pandas as pd
-from scipy.signal import savgol_filter
-
 from bokeh.io import curdoc
-from bokeh.layouts import row, column
-from bokeh.models import ColumnDataSource, DataRange1d, Select
-from bokeh.palettes import Blues4
+from bokeh.models import CustomJS, Select, Slider, ColumnDataSource, BoxAnnotation
+from bokeh.layouts import grid
 from bokeh.plotting import figure
 
-STATISTICS = ['record_min_temp', 'actual_min_temp', 'average_min_temp', 'average_max_temp', 'actual_max_temp', 'record_max_temp']
+import numpy as np
+import pickle
 
-def get_dataset(src, name, distribution):
-    df = src[src.airport == name].copy()
-    del df['airport']
-    df['date'] = pd.to_datetime(df.date)
-    # timedelta here instead of pd.DateOffset to avoid pandas bug < 0.18 (Pandas issue #11925)
-    df['left'] = df.date - datetime.timedelta(days=0.5)
-    df['right'] = df.date + datetime.timedelta(days=0.5)
-    df = df.set_index(['date'])
-    df.sort_index(inplace=True)
-    if distribution == 'Smoothed':
-        window, order = 51, 3
-        for key in STATISTICS:
-            df[key] = savgol_filter(df[key], window, order)
+import sys
 
-    return ColumnDataSource(data=df)
+data_folder = sys.argv[1]
+sort_type = sys.argv[2]
 
-def make_plot(source, title):
-    plot = figure(x_axis_type="datetime", plot_width=800, tools="", toolbar_location=None)
-    plot.title.text = title
+# Load metadata of given simulation
+with open(f'{data_folder}general.data', 'rb') as f:
+    metadata = pickle.load(f)
+num_inh = metadata['num_inh']
+input_rate = metadata['input_rate']
+ei_conn = metadata['e->i p']
+#ffi_weight = metadata['']
+num_exc = metadata['num_exc']
+num_channels = metadata['num_channels']
+rasters = np.load(f'{data_folder}rasters.npz')
+traces = np.load(f'{data_folder}traces.npz')
+matrices = np.load(f'{data_folder}matrices.npz', allow_pickle=True)
+rf = matrices['rf']
+rf = np.reshape(rf, (num_channels, num_exc, -1))
+source = ColumnDataSource(data=dict(weights=rf[:, 0, 0],
+                                    channels=range(np.shape(rf)[0]),
+                                    colors=48*['red']+48*['green']+48*['blue']
+                                    ))
 
-    plot.quad(top='record_max_temp', bottom='record_min_temp', left='left', right='right',
-              color=Blues4[2], source=source, legend="Record")
-    plot.quad(top='average_max_temp', bottom='average_min_temp', left='left', right='right',
-              color=Blues4[1], source=source, legend="Average")
-    plot.quad(top='actual_max_temp', bottom='actual_min_temp', left='left', right='right',
-              color=Blues4[0], alpha=0.5, line_color="black", source=source, legend="Actual")
+# Plots
+p = figure(plot_width=900, plot_height=400, x_range=(0, 143), y_range=(0, 16))
+p.vbar(x='channels', top='weights', width=0.8, color='colors', source=source)
 
-    # fixed attributes
-    plot.xaxis.axis_label = None
-    plot.yaxis.axis_label = "Temperature (F)"
-    plot.axis.axis_label_text_font_style = "bold"
-    plot.x_range = DataRange1d(range_padding=0.0)
-    plot.grid.grid_line_alpha = 0.3
+# Create widgets
+neuron_select = Select(title="Neuron:", value="0", options=[str(x) for x in range(num_exc)])
+time_slider = Slider(start=0, end=np.shape(rf)[2]-1, value=0, step=1, title="Time (ms)")
 
-    return plot
+def update_plot(attr, old, new):
+    source.data = dict(weights=rf[:, int(neuron_select.value), time_slider.value],
+                       channels=range(np.shape(rf)[0]),
+                       colors=48*['red']+48*['green']+48*['blue']
+                       )
 
-def update_plot(attrname, old, new):
-    city = city_select.value
-    plot.title.text = "Weather data for " + cities[city]['title']
+time_slider.on_change('value', update_plot)
+neuron_select.on_change('value', update_plot)
 
-    src = get_dataset(df, cities[city]['airport'], distribution_select.value)
-    source.data.update(src.data)
+layout = grid([
+        [neuron_select, p],
+        [time_slider]
+    ])
 
-city = 'Austin'
-distribution = 'Discrete'
+curdoc().add_root(layout)
 
-cities = {
-    'Austin': {
-        'airport': 'AUS',
-        'title': 'Austin, TX',
-    },
-    'Boston': {
-        'airport': 'BOS',
-        'title': 'Boston, MA',
-    },
-    'Seattle': {
-        'airport': 'SEA',
-        'title': 'Seattle, WA',
-    }
-}
-
-city_select = Select(value=city, title='City', options=sorted(cities.keys()))
-distribution_select = Select(value=distribution, title='Distribution', options=['Discrete', 'Smoothed'])
-
-df = pd.read_csv(join(dirname(__file__), 'data/2015_weather.csv'))
-source = get_dataset(df, cities[city]['airport'], distribution)
-plot = make_plot(source, "Weather data for " + cities[city]['title'])
-
-city_select.on_change('value', update_plot)
-distribution_select.on_change('value', update_plot)
-
-controls = column(city_select, distribution_select)
-
-curdoc().add_root(row(plot, controls))
-curdoc().title = "Weather"
